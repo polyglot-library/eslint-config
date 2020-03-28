492a22fbabb3ef2f9350eff89584b56c264412f3
Wallysson Nunes <wally.nm@gmail.com>
2020-03-28 02:54:36 -0300

    Adds support for optional chainning

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ module.exports = {
     "google"
   ],
   "plugins": [
+    "babel",
     "react"
   ],
   rules: {
-    "object-curly-spacing": ["error", "always"]
+    "object-curly-spacing": ["error", "always"],
+    "babel/camelcase": [ "error",
+      {
+        "ignoreDestructuring":true,
+        "properties":"never"
+      }
+    ],
+    "max-len":[ "error", {
+      "code": 100,
+      "tabWidth": 4,
+      "ignoreComments": true,
+      "ignoreTemplateLiterals": true,
+      "ignoreRegExpLiterals": true,
+      "ignoreUrls": true
+    }]
   },
 };
